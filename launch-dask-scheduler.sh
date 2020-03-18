238d6882da6a0fc54896890714e47cb9bb3891f3
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-03-18 14:28:41 +0800

    Create reverse tunnel for dashboard (try again)

--- launch-dask-scheduler.sh
+++ launch-dask-scheduler.sh
@@ -10,8 +10,11 @@
 # -m    define mail message condition, abort/begin/terminate
 
 terminate_ssh() {
-    echo "SIGTERM, terminating reverse tunnel"
+    echo "SIGTERM, terminating the reverse tunnel"
     ssh -S $HOME/scheduler.socket -O exit warp
+    rm -f $HOME/scheduler.socket
+    ssh -S $HOME/dashboard.socket -O exit warp
+    rm -f $HOME/dashboard.socket
 }
 
 # prepare path
@@ -20,6 +23,7 @@ source "${conda_base}/etc/profile.d/conda.sh"
 # establish reverse tunnel to the head node
 # .. use control file in master mode for control
 ssh -fN -M -S $HOME/scheduler.socket -R 8786:localhost:8786 warp # scheduler
+ssh -fN -M -S $HOME/dashboard.socket -R 8787:localhost:8787 warp # scheduler
 
 # launch environment
 conda activate pbs
@@ -27,8 +31,7 @@ conda activate pbs
 SCHEDULER=$HOME/scheduler.json
 rm -f $SCHEDULER
 
-# scheulder on rank 0, workers/nannies on remaining ranks
-# so we restrict to 1 rank for scheduler job
 dask-scheduler \
+    --dashboard --no-show \
     --local-directory /scratch/$USER \
     --scheduler-file $SCHEDULER
